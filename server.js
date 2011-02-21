cd9dbca9ba2f88d789f74be1bf103719bf95fb92
Ryan W Tenney <ryan@Metroid.martellotech.local>
2011-02-21 16:10:23 -0600

    Initial commit.

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express.createServer();
+
+
+app.configure(function () {
+    app.use(express.staticProvider(__dirname + "/public"));
+    app.use(express.errorHandler({dumpExceptions:true}));
+    app.use(express.bodyDecoder());
+    app.use(express.methodOverride());
+});
+
+
+var tabs = {};
+
+
+app.get("/tabs/", function (req, res) {
+    res.send(tabs);
+});
+
+app.post("/tabs/", function (req, res) {
+    tabs = req.body;
+    res.end();
+});
+
+
+app.get("/tab/:id", function (req, res) {
+    var id = req.params.id;
+    if (tabs[id]) {
+        res.send(tabs[id]);
+    } else {
+        res.writeHead(404);
+    }
+});
+
+app.post("/tab/:id", function (req, res) {
+    tabs[req.params.id] = req.body;
+    res.end();
+});
+
+app.put("/tab/:id", function (req, res) {
+    tabs[req.params.id] = req.body;
+    res.end();
+});
+
+app.del("/tab/:id", function (req, res) {
+    delete tabs[req.params.id];
+    res.end();
+});
+
+
+app.listen(22111);
